9bfe1e01c7d33fc50acf3b5031c9b4b5d261e267
Jari Aalto <jari.aalto@cante.net>
2012-02-01 11:36:28 +0200

    test.sh: Add bz2 xz tests

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 pwd=$(cd $(dirname $0) && pwd)
-PATH="$pwd/../.inst/usr/bin:$PATH"
+export PATH="$pwd/../.inst/usr/bin:$PATH"
 
 INC=$(perl -e 'print join qq(:), @INC')
 LIB=$(cd $pwd/../.inst/usr/lib/perl5/vendor_perl/5*/ && pwd)
-PERL5LIB="$LIB:$INC"
+export PERL5LIB="$LIB:$INC"
 
 ROOT=/tmp/pristine-tar
 
@@ -20,8 +20,17 @@ set -e
 which pristine-tar
 
 cd $ROOT
-tar -zcvf package-1.1.tar.gz package-1.1
-tar -zcvf package-1.2.tar.gz package-1.2
+
+tar --gzip -cf package-1.1.tar.gz package-1.1
+tar --gzip -cf package-1.2.tar.gz package-1.2
+
+tar --bzip2 -cf packagebz-1.1.tar.bz2 package-1.1
+tar --bzip2 -cf packagebz-1.2.tar.bz2 package-1.2
+
+tar --xz -cf packagexz-1.1.tar.xz package-1.1
+tar --xz -cf packagexz-1.2.tar.xz package-1.2
+
+ls -la
 
 cd $ROOT/package-1.1
 
@@ -38,14 +47,27 @@ git tag upstream/1.2
 
 pristine-tar commit ../package-1.1.tar.gz
 pristine-tar commit ../package-1.2.tar.gz
+
+pristine-tar commit ../packagebz-1.1.tar.bz2
+pristine-tar commit ../packagebz-1.2.tar.bz2
+
+pristine-tar commit ../packagexz-1.1.tar.xz
+pristine-tar commit ../packagexz-1.2.tar.xz
+
 git checkout pristine-tar
 ls -l
 
-rm ../package-*.tar.gz
-git checkout pristine-tar
+rm ../package-*.tar.*
 
 pristine-tar checkout ../package-1.1.tar.gz
 pristine-tar checkout ../package-1.2.tar.gz
+
+pristine-tar checkout ../packagebz-1.1.tar.bz2
+pristine-tar checkout ../packagebz-1.2.tar.bz2
+
+pristine-tar checkout ../packagexz-1.1.tar.xz
+pristine-tar checkout ../packagexz-1.2.tar.xz
+
 cd ..
 ls -la
 
